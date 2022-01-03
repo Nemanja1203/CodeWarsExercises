eb4b2324f38ace94cbbf4517072a85ef15622aba
Nemanja Milosevic <nemanja.milosevic@supper.london>
2022-01-03 21:36:49 +0100

    Kata Canal Management

---
+++ tests/KataPersistence.UnitTests/Kata_012_PickPeaks_Tests.cs
@@ -0,0 +1,140 @@
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace KataExercises.UnitTests;
+
+public class Kata_012_PickPeaks_Tests
+{
+    private static string[] _msg =
+    {
+        "should support finding peaks",
+        "should support finding peaks, but should ignore peaks on the edge of the array",
+        "should support finding peaks; if the peak is a plateau, it should only return the position of the first element of the plateau",
+        "should support finding peaks; if the peak is a plateau, it should only return the position of the first element of the plateau",
+        "should support finding peaks, but should ignore peaks on the edge of the array"
+    };
+
+    private static int[][] _array =
+    {
+        new int[]{1,2,3,6,4,1,2,3,2,1},
+        new int[]{3,2,3,6,4,1,2,3,2,1,2,3},
+        new int[]{3,2,3,6,4,1,2,3,2,1,2,2,2,1},
+        new int[]{2,1,3,1,2,2,2,2,1},
+        new int[]{2,1,3,1,2,2,2,2}
+    };
+
+    private static int[][] _posS =
+    {
+        new int[]{3,7},
+        new int[]{3,7},
+        new int[]{3,7,10},
+        new int[]{2,4},
+        new int[]{2}
+    };
+
+    private static int[][] peaksS =
+    {
+        new int[]{6,3},
+        new int[]{6,3},
+        new int[]{6,3,2},
+        new int[]{3,2},
+        new int[]{3}
+    };
+
+    [Test]
+    public void SampleTests()
+    {
+        for (int n = 0; n < _msg.Length; n++)
+        {
+            int[] p1 = _posS[n], p2 = peaksS[n];
+            var expected = new Dictionary<string, List<int>>()
+            {
+                ["pos"] = p1.ToList(),
+                ["peaks"] = p2.ToList()
+            };
+            var actual = Kata_012_PickPeaks.GetPeaks(_array[n]);
+            Assert.AreEqual(expected, actual, _msg[n]);
+        }
+    }
+
+    [Test]
+    public void GeatPeaks_NoPeaks_DoesntFindAnyPeaks()
+    {
+        // Arrange
+        var arr = new int[] { 1, 2, 3, 4, 5 };
+
+        // Act
+        var res = Kata_012_PickPeaks.GetPeaks(arr);
+
+        // Assert
+        //res["pos"].Count
+        Assert.AreEqual(0, res["pos"].Count);
+        Assert.AreEqual(0, res["peaks"].Count);
+    }
+
+    [Test]
+    public void GetPeaks_OnePeak_FindsOnePeak()
+    {
+        // Arrange
+        var arr = new int[] { 1, 2, 1 };
+
+        // Act
+        var res = Kata_012_PickPeaks.GetPeaks(arr);
+
+        // Assert
+        Assert.AreEqual(1, res["pos"].Count);
+        Assert.AreEqual(1, res["pos"].First());
+
+        Assert.AreEqual(1, res["peaks"].Count);
+        Assert.AreEqual(2, res["peaks"].First());
+    }
+
+    [Test]
+    public void GetPeaks_PeakAtPlateau_FindsOnePeak()
+    {
+        // Arrange
+        var arr = new int[] { 1, 2, 2, 1 };
+
+        // Act
+        var res = Kata_012_PickPeaks.GetPeaks(arr);
+
+        // Assert
+        Assert.AreEqual(1, res["pos"].Count);
+        Assert.AreEqual(1, res["pos"].First());
+
+        Assert.AreEqual(1, res["peaks"].Count);
+        Assert.AreEqual(2, res["peaks"].First());
+    }
+
+    [Test]
+    public void GetPeaks_PeakAtPlateau3_FindsOnePeak()
+    {
+        // Arrange
+        var arr = new int[] { 1, 2, 3, 3, 3, 2, 1 };
+
+        // Act
+        var res = Kata_012_PickPeaks.GetPeaks(arr);
+
+        // Assert
+        Assert.AreEqual(1, res["pos"].Count);
+        Assert.AreEqual(2, res["pos"].First());
+
+        Assert.AreEqual(1, res["peaks"].Count);
+        Assert.AreEqual(3, res["peaks"].First());
+    }
+
+    [Test]
+    public void GetPeaks_PlateauNoPieak_DoesntFindAnyPeaks()
+    {
+        // Arrange
+        var arr = new int[] { 1, 2, 2, 3 };
+
+        // Act
+        var res = Kata_012_PickPeaks.GetPeaks(arr);
+
+        // Assert
+        Assert.AreEqual(0, res["pos"].Count);
+        Assert.AreEqual(0, res["peaks"].Count);
+    }
+}
---
+++ tests/KataPersistence.UnitTests/Kata_013_CanalManagement_Tests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+
+namespace KataExercises.UnitTests;
+
+[TestFixture]
+public class Kata_013_CanalManagement_Tests
+{
+
+    [TestCase(new uint[] { 2, 3, 6, 1 }, new uint[] { 1, 2 }, 7, 38)]
+    [TestCase(new uint[] { 1, 2, 3, 4, 5, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 }, new uint[] { 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 }, 30, 868)]
+    public void SampleTests(uint[] lowQueue, uint[] highQueue, int lockLength, int expected)
+      => Tester.Act(lowQueue, highQueue, (uint)lockLength, (uint)expected);
+
+    [TestCase(new uint[] { 4, 1, 8, 2, 6 }, new uint[0], 8, 54)]
+    public void NoHighQueueTest(uint[] lowQueue, uint[] highQueue, int lockLength, int expected)
+      => Tester.Act(lowQueue, highQueue, (uint)lockLength, (uint)expected);
+
+    [TestCase(new uint[0], new uint[] { 4, 4, 7, 2, 8, 5 }, 8, 80)]
+    public void NoLowQueueTest(uint[] lowQueue, uint[] highQueue, int lockLength, int expected)
+      => Tester.Act(lowQueue, highQueue, (uint)lockLength, (uint)expected);
+
+    [TestCase(new uint[0], new uint[0], 10, 0)]
+    public void NoQueuesTest(uint[] lowQueue, uint[] highQueue, int lockLength, int expected)
+      => Tester.Act(lowQueue, highQueue, (uint)lockLength, (uint)expected);
+}
+
+internal static class Tester
+{
+
+    public static void Act(uint[] lowQueue, uint[] highQueue, uint lockLength, uint expected)
+    {
+        var low = $"[{string.Join(",", lowQueue)}]";
+        var high = $"[{string.Join(",", highQueue)}]";
+        var msg = $"Incorrect answer for\n\t:low = {low}\n\thigh={high}";
+        var actual = Kata_013_CanalManagement.CanalMania(lowQueue, highQueue, lockLength);
+        Assert.AreEqual(expected, actual, msg);
+    }
+}
