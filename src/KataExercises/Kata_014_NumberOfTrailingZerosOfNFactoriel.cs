7b35fa74501c840adb797d2c226e0ec9942bdf33
Nemanja Milosevic <nemanja.milosevic@supper.london>
2022-01-04 18:49:47 +0100

    Kata Number of trailing zeros of n factoriel

---
+++ src/KataExercises/Kata_014_NumberOfTrailingZerosOfNFactoriel.cs
@@ -0,0 +1,36 @@
+﻿//Write a program that will calculate the number of trailing zeros in a factorial of a given number.
+
+//N! = 1 * 2 * 3 * ... * N
+
+//Be careful 1000! has 2568 digits...
+
+//For more info, see: http://mathworld.wolfram.com/Factorial.html
+
+//Examples
+//zeros(6) = 1
+//# 6! = 1 * 2 * 3 * 4 * 5 * 6 = 720 --> 1 trailing zero
+
+//zeros(12) = 2
+//# 12! = 479001600 --> 2 trailing zeros
+//Hint: You're not meant to calculate the factorial. Find another way to find the number of zeros.
+
+namespace KataExercises;
+
+public class Kata_014_NumberOfTrailingZerosOfNFactoriel
+{
+    public static int TrailingZeros(int n)
+    {
+        //https://mathworld.wolfram.com/Factorial.html
+        int kMin = 1;
+        int kMax = (int)Math.Floor(Math.Log(n, 5));
+
+        int z = 0;
+
+        for (int k = kMin; k <= kMax; k++)
+        {
+            z += (int)Math.Floor(n / (Math.Pow(5, k)));
+        }
+
+        return z;
+    }
+}
---
+++ tests/KataPersistence.UnitTests/Kata_014_NumberOfTrailingZerosOfNFactoriel_Tests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace KataExercises.UnitTests;
+
+[TestFixture]
+public class Kata_014_NumberOfTrailingZerosOfNFactoriel_Tests
+{
+    [Test]
+    public void BasicTests()
+    {
+        Assert.AreEqual(1, Kata_014_NumberOfTrailingZerosOfNFactoriel.TrailingZeros(5));
+        Assert.AreEqual(6, Kata_014_NumberOfTrailingZerosOfNFactoriel.TrailingZeros(25));
+        Assert.AreEqual(131, Kata_014_NumberOfTrailingZerosOfNFactoriel.TrailingZeros(531));
+    }
+}
