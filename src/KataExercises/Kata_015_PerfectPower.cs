fe84eccdc07f527fc8d4588028dbf36ad9b19003
Nemanja Milosevic <nemanja.milosevic@supper.london>
2022-01-05 19:21:11 +0100

    Kata Perfect Power

---
+++ src/KataExercises/Kata_015_PerfectPower.cs
@@ -0,0 +1,60 @@
+﻿//A perfect power is a classification of positive integers:
+
+//In mathematics, a perfect power is a positive integer that can be expressed
+//as an integer power of another positive integer.
+//More formally, n is a perfect power if there exist natural numbers m > 1, and k > 1 such that m^k = n.
+
+//Your task is to check wheter a given integer is a perfect power.
+//If it is a perfect power, return a pair m and k with mk = n as a proof.
+//Otherwise return Nothing, Nil, null, NULL, None or your language's equivalent.
+
+//Note: For a perfect power, there might be several pairs. For example 81 = 3^4 = 9^2,
+//so (3, 4) and (9,2) are valid solutions. However, the tests take care of this,
+//so if a number is a perfect power, return any pair that proves it.
+
+//Examples
+//IsPerfectPower(4) => (2, 2)
+//IsPerfectPower(5) => null
+//IsPerfectPower(8) => (2, 3)
+//IsPerfectPower(9) => (3, 2)
+
+namespace KataExercises;
+
+public class Kata_015_PerfectPower
+{
+    public static (int, int)? IsPerfectPower(int n)
+    {
+        if (n < 4)
+        {
+            return null;
+        }
+
+        var divisorsOfN = new List<int>();
+        int maxDevisor = (int)Math.Ceiling(Math.Sqrt(n));
+        for (int i = 2; i <= maxDevisor; i++)
+        {
+            if (((double)n / i) % 1 == 0)
+            {
+                divisorsOfN.Add(i);
+            }
+        }
+
+        int kMax = (int)Math.Ceiling(Math.Log2(n));
+        foreach (var m in divisorsOfN)
+        {
+            int k = 2;
+            int result;
+            do
+            {
+                result = (int)Math.Pow(m, k);
+                if (result == n)
+                {
+                    return new(m, k);
+                }
+                k++;
+            } while (k <= kMax);
+        }
+
+        return null;
+    }
+}
---
+++ tests/KataPersistence.UnitTests/Kata_015_PerfectPower_Tests.cs
@@ -0,0 +1,65 @@
+﻿using NUnit.Framework;
+
+namespace KataExercises.UnitTests;
+
+public class Kata_015_PerfectPower_Tests
+{
+    [Test]
+    public void Test0()
+    {
+        Assert.IsNull(Kata_015_PerfectPower.IsPerfectPower(0), "0 is not a perfect number");
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.IsNull(Kata_015_PerfectPower.IsPerfectPower(1), "1 is not a perfect number");
+    }
+
+    [Test]
+    public void Test2()
+    {
+        Assert.IsNull(Kata_015_PerfectPower.IsPerfectPower(2), "2 is not a perfect number");
+    }
+
+    [Test]
+    public void Test3()
+    {
+        Assert.IsNull(Kata_015_PerfectPower.IsPerfectPower(3), "3 is not a perfect number");
+    }
+
+    [Test]
+    public void Test4()
+    {
+        Assert.AreEqual((2, 2), Kata_015_PerfectPower.IsPerfectPower(4), "4 = 2^2");
+    }
+
+    [Test]
+    public void Test5()
+    {
+        Assert.IsNull(Kata_015_PerfectPower.IsPerfectPower(5), "5 is not a perfect power");
+    }
+
+    [Test]
+    public void Test8()
+    {
+        Assert.AreEqual((2, 3), Kata_015_PerfectPower.IsPerfectPower(8), "8 = 2^3");
+    }
+
+    [Test]
+    public void Test9()
+    {
+        Assert.AreEqual((3, 2), Kata_015_PerfectPower.IsPerfectPower(9), "9 = 3^2");
+    }
+
+    [Test]
+    public void TestUpTo500()
+    {
+        var pp = new int[] { 4, 8, 9, 16, 25, 27, 32, 36, 49, 64, 81, 100, 121, 125,
+            128, 144, 169, 196, 216, 225, 243, 256, 289, 324, 343, 361, 400, 441, 484 };
+        foreach (var i in pp)
+        {
+            Assert.IsNotNull(Kata_015_PerfectPower.IsPerfectPower(i), $"{i} is a perfect power");
+        }
+    }
+}
