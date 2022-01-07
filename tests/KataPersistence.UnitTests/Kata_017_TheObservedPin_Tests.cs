0f3ab9ce97ddd8cfd20f2b2d5ffbbeb498231c75
Nemanja Milosevic <nemanja.milosevic@supper.london>
2022-01-07 21:04:56 +0100

    Kata Observed pin

---
+++ tests/KataPersistence.UnitTests/Kata_017_TheObservedPin_Tests.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace KataExercises.UnitTests;
+
+public class Kata_017_TheObservedPin_Tests
+{
+    [Test]
+    public void TestBasic()
+    {
+        var expectations = new Dictionary<string, string[]>
+        {
+            {"8", new[] {"5", "7", "8", "9", "0"}},
+            {"11", new[] {"11", "22", "44", "12", "21", "14", "41", "24", "42"}},
+            {
+                "369",
+                new[]
+                {
+                    "339", "366", "399", "658", "636", "258", "268", "669", "668", "266", "369", "398", "256",
+                    "296", "259", "368", "638", "396", "238", "356", "659", "639", "666", "359", "336", "299",
+                    "338", "696", "269", "358", "656", "698", "699", "298", "236", "239"
+                }
+            }
+        };
+
+        foreach (var pin in expectations)
+        {
+            CollectionAssert.AreEquivalent(pin.Value, Kata_017_TheObservedPin.GetPINs(pin.Key), "PIN: " + pin);
+        }
+    }
+
+    [Test]
+    public void Test2()
+    {
+        // Arrange
+        var pin = "8";
+
+        // Act
+        var res = Kata_017_TheObservedPin.GetPINs("8");
+
+        // Assert
+        CollectionAssert.AreEquivalent(pin, res);
+    }
+}
