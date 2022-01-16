a960bffdeac48f234642a29e55077d268643fb23
Nemanja Milosevic <nemanja.milosevic@supper.london>
2022-01-16 19:44:02 +0100

    Kata ParseIntReloaded

---
+++ src/KataExercises/Kata_018_ParseIntReloaded.cs
@@ -0,0 +1,90 @@
+﻿//In this kata we want to convert a string into an integer. The strings simply represent the numbers in words.
+
+//Examples:
+
+//"one" => 1
+//"twenty" => 20
+//"two hundred forty-six" => 246
+//"seven hundred eighty-three thousand nine hundred and nineteen" => 783919
+//Additional Notes:
+
+//The minimum number is "zero" (inclusively)
+//The maximum number, which must be supported is 1 million (inclusively)
+//The "and" in e.g. "one hundred and twenty-four" is optional, in some cases it's present and in others it's not
+//All tested numbers are valid, you don't need to validate them
+
+// https://www.c-sharpcorner.com/Blogs/convert-words-to-numbers-in-c-sharp
+namespace KataExercises;
+
+public class Kata_018_ParseIntReloaded
+{
+    private static readonly Dictionary<string, int> _wordToNumberMapping = new Dictionary<string, int>()
+    {
+
+        { "zero", 0 },
+        { "one", 1 },
+        { "two", 2 },
+        { "three", 3 },
+        { "four", 4 },
+        { "five", 5 },
+        { "six", 6 },
+        { "seven", 7 },
+        { "eight", 8 },
+        { "nine", 9 },
+        { "ten", 10 },
+        { "eleven", 11 },
+        { "twelve", 12 },
+        { "thirteen", 13 },
+        { "fourteen", 14 },
+        { "fifteen", 15 },
+        { "sixteen", 16 },
+        { "seventeen", 17 },
+        { "eighteen", 18 },
+        { "nineteen", 19 },
+        { "twenty", 20 },
+        { "thirty", 30 },
+        { "forty", 40 },
+        { "fifty", 50 },
+        { "sixty", 60 },
+        { "seventy", 70 },
+        { "eighty", 80 },
+        { "ninety", 90 },
+        { "hundred", 100 },
+        { "thousand", 1_000 },
+        { "million", 1_000_000 }
+    };
+
+    public static int ParseInt(string s)
+    {
+        s = s.Replace("-", " ");
+        s = s.Replace(" and ", " ");
+        s = s.Trim();
+
+        var sSplit = s.Split(" ");
+        int res = 0;
+        int baseNumber = 0;
+
+        foreach (var item in sSplit)
+        {
+            if (!string.IsNullOrEmpty(item))
+            {
+                var n = _wordToNumberMapping[item];
+                if (n >= 1_000)
+                {
+                    res += baseNumber * n;
+                    baseNumber = 0;
+                }
+                else if (n >= 100)
+                {
+                    baseNumber *= n;
+                }
+                else
+                {
+                    baseNumber += n;
+                }
+            }
+        }
+        res += baseNumber;
+        return res;
+    }
+}
---
+++ tests/KataPersistence.UnitTests/Kata_018_ParseIntReloaded_Tests.cs
@@ -0,0 +1,120 @@
+﻿using NUnit.Framework;
+
+namespace KataExercises.UnitTests;
+
+public class Kata_018_ParseIntReloaded_Tests
+{
+    [Test]
+    public void FixedTests()
+    {
+        Assert.AreEqual(1, Kata_018_ParseIntReloaded.ParseInt("one"));
+        Assert.AreEqual(20, Kata_018_ParseIntReloaded.ParseInt("twenty"));
+        Assert.AreEqual(246, Kata_018_ParseIntReloaded.ParseInt("two hundred forty-six"));
+    }
+
+    [Test]
+    public void ParseInt_One_1()
+    {
+        // Arrange
+        var numberAsString = "one";
+
+        // Act
+        var res = Kata_018_ParseIntReloaded.ParseInt(numberAsString);
+
+        // Assert
+        Assert.AreEqual(1, res);
+    }
+
+    [Test]
+    public void ParseInt_TwoHundredFortySix_246()
+    {
+        // Arrange
+        var numberAsString = "two hundred forty-six";
+
+        // Act
+        var res = Kata_018_ParseIntReloaded.ParseInt(numberAsString);
+
+        // Assert
+        Assert.AreEqual(246, res);
+    }
+
+    [Test]
+    public void ParseInt_TwentyTwoHundred_2200()
+    {
+        // Arrange
+        var numberAsString = "twenty two hundred";
+
+        // Act
+        var res = Kata_018_ParseIntReloaded.ParseInt(numberAsString);
+
+        // Assert
+        Assert.AreEqual(2200, res);
+    }
+
+    [Test]
+    public void ParseInt_TwoThousandTwoHundred_2200()
+    {
+        // Arrange
+        var numberAsString = "two thousand two hundred";
+
+        // Act
+        var res = Kata_018_ParseIntReloaded.ParseInt(numberAsString);
+
+        // Assert
+        Assert.AreEqual(2200, res);
+    }
+
+    [Test]
+    public void ParseInt__666666()
+    {
+        // Arrange
+        var numberAsString = "six hundred sixty-six thousand six hundred sixty-six";
+
+        // Act
+        var res = Kata_018_ParseIntReloaded.ParseInt(numberAsString);
+
+        // Assert
+        Assert.AreEqual(666_666, res);
+    }
+
+    [Test]
+    public void ParseInt__864452()
+    {
+        // Arrange
+        var numberAsString = "eight hundred and sixty-four thousand four hundred and fifty-two";
+
+        // Act
+        var res = Kata_018_ParseIntReloaded.ParseInt(numberAsString);
+
+        // Assert
+        Assert.AreEqual(864_452, res);
+    }
+    
+    [Test]
+    public void ParseInt_OneThousandThreeHundredAndThirtySeven_1337()
+    {
+        // Arrange
+        var numberAsString = "one thousand three hundred and thirty-seven";
+
+        // Act
+        var res = Kata_018_ParseIntReloaded.ParseInt(numberAsString);
+
+        // Assert
+        Assert.AreEqual(1_337, res);
+    }
+    
+    [Test]
+    public void ParseInt_SevenHundredThousand_700000()
+    {
+        // Arrange
+        var numberAsString = "seven hundred thousand";
+
+        // Act
+        var res = Kata_018_ParseIntReloaded.ParseInt(numberAsString);
+
+        // Assert
+        Assert.AreEqual(700_000, res);
+    }
+
+
+}
