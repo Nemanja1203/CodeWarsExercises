119e1e9ce6e2ca1b3d288ba4648971790bc62da8
Nemanja Milosevic <nemanja.milosevic@supper.london>
2022-01-24 19:30:42 +0100

    Kata RangeExtraction

---
+++ src/KataExercises/Kata_019_RangeExtraction.cs
@@ -0,0 +1,50 @@
+﻿namespace KataExercises;
+
+public class Kata_019_RangeExtraction
+{
+    public static string Extract(int[] args)
+    {
+        List<int> range = new List<int>();
+        List<string> result = new List<string>();
+        range.Add(args[0]);
+
+        for (int i = 1; i < args.Length; i++)
+        {
+            var el = args[i];
+
+            if (range.Count == 0)
+            {
+                range.Add(el);
+            }
+            else if (el == range.Last() + 1)
+            {
+                range.Add(el);
+            }
+            else
+            {
+                if (range.Count > 2)
+                {
+                    result.Add("" + range.First() + "-" + range.Last());
+                }
+                else
+                {
+                    result.Add(string.Join(",", range.Select(x => x.ToString())));
+                }
+                range.Clear();
+                range.Add(el);
+            }
+        }
+
+        if (range.Count > 2)
+        {
+            result.Add("" + range.First() + "-" + range.Last());
+        }
+        else
+        {
+            result.Add(string.Join(",", range.Select(x => x.ToString())));
+        }
+
+        var res = string.Join(",", result);
+        return res;
+    }
+}
---
+++ tests/KataPersistence.UnitTests/Kata_019_RangeExtraction_Tests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace KataExercises.UnitTests;
+
+[TestFixture]
+public class Kata_019_RangeExtraction_Tests
+{
+    [Test(Description = "Simple tests")]
+    public void SimpleTests()
+    {
+        Assert.AreEqual("1,2", Kata_019_RangeExtraction.Extract(new[] { 1, 2 }));
+        Assert.AreEqual("1-3", Kata_019_RangeExtraction.Extract(new[] { 1, 2, 3 }));
+
+        Assert.AreEqual(
+            "-6,-3-1,3-5,7-11,14,15,17-20",
+            Kata_019_RangeExtraction.Extract(new[] { -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20 })
+        );
+
+        Assert.AreEqual(
+            "-3--1,2,10,15,16,18-20",
+            Kata_019_RangeExtraction.Extract(new[] { -3, -2, -1, 2, 10, 15, 16, 18, 19, 20 })
+        );
+    }
+}
