3c09b04d2dbfdd31751c88d6aadaa7dca6260e6e
Nemanja Milosevic <nemanja.milosevic@supper.london>
2022-01-25 23:06:44 +0100

    Kata BefungeInterpreter

---
+++ tests/KataPersistence.UnitTests/Kata_020_BefungeInterpreter_Tests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace KataExercises.UnitTests;
+
+[TestFixture]
+public class Kata_020_BefungeInterpreter_Tests
+{
+    [Test]
+    public void Tests()
+    {
+        Assert.AreEqual(
+                "123456789",
+                new Kata_020_BefungeInterpreter().Interpret(">987v>.v\nv456<  :\n>321 ^ _@"));
+    }
+}
